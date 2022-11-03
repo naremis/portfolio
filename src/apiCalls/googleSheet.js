45e55e82082a7f55ca2fb73bf46cd78d4c965ec6
RohanTaufique <rohantaufique@gmail.com>
2022-11-03 16:11:12 +0100

    env variables updated

--- src/apiCalls/googleSheet.js
+++ src/apiCalls/googleSheet.js
@@ -2,10 +2,10 @@ const { GoogleSpreadsheet } = require("google-spreadsheet");
 
 export const saveResponse = async (data) => {
   try {
-    const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
+    const doc = new GoogleSpreadsheet(process.env.NEXT_PUBLIC_GOOGLE_SHEET_ID);
     await doc.useServiceAccountAuth({
-      client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-      private_key: process.env.GOOGLE_PRIVATE_KEY,
+      client_email: process.env.NEXT_PUBLIC_GOOGLE_SERVICE_ACCOUNT_EMAIL,
+      private_key: process.env.NEXT_PUBLIC_GOOGLE_PRIVATE_KEY,
     });
     await doc.loadInfo(); // loads document properties and worksheets
 
